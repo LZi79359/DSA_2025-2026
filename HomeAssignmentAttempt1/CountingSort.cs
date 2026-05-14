1a076627bd5000e551ad6d3bf0926f190171103a
LZi79359 <lukezammit2007@gmail.com>
2026-05-14 23:09:50 +0200

    Coded Task 1.1 of Home Assignment

---
+++ HomeAssignmentAttempt1/CountingSort.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeAssignmentAttempt1
+{
+    /// <summary>
+    /// Counting Sort
+    /// Best Case = O(n+k)
+    /// Average Case = O(n+k)
+    /// Worst Case = O(n+k)
+    /// </summary>
+    public class CountingSort
+    {
+        public static int[] Sort(int[] unsortedArray)
+        {
+            int[] arrayCopy = (int[])unsortedArray.Clone();
+            CountSortHelper(arrayCopy);
+            return arrayCopy;
+        }
+
+        private static void CountSortHelper(int[] ints)
+        {
+            int max = ints[0];
+
+            foreach (int i in ints)
+            {
+                if (max < i) max = i;
+            }
+
+            int[] count = new int[max + 1];
+
+            foreach (int i in ints)
+            {
+                count[i]++;
+            }
+
+            int rank = 0;
+            for (int i = 0; i <= max; i++)
+            {
+                for (int j = 0; j < count[i]; j++)
+                {
+                    ints[rank] = i;
+                    rank++;
+                }
+            }
+        }
+
+    }
+}
--- HomeAssignmentAttempt1/MergeSort.cs
+++ HomeAssignmentAttempt1/MergeSort.cs
@@ -6,7 +6,66 @@ using System.Threading.Tasks;
 
 namespace HomeAssignmentAttempt1
 {
-    internal class MergeSort
+    /// <summary>
+    /// Merge Sort Algorithm
+    /// Best Case: O(n log n)
+    /// Avg Case: O(n log n)
+    /// Worst Case: O(n log n)
+    /// </summary>
+    public class MergeSort
     {
+        public static int[] Sort(int[] unsortedArray)
+        {
+            int[] arrayCopy = (int[])unsortedArray.Clone();
+            MergeSortHelper(arrayCopy, 0, arrayCopy.Length - 1);
+            return arrayCopy;
+        }
+
+        private static void MergeSortHelper(int[] ints, int left, int right )
+        {
+            if (left >= right) return;
+            int midPoint = (left + right) / 2;
+            MergeSortHelper(ints, left, midPoint);
+            MergeSortHelper(ints, midPoint+1, right);
+            
+            int[] tempArray = new int[right-left+1];
+            int leftPointer = left;
+            int rightPointer = midPoint + 1;
+            int rank = 0;
+
+            while(leftPointer <= midPoint && rightPointer <= right)
+            {
+                if (ints[leftPointer] < ints[rightPointer])
+                {
+                    tempArray[rank] = ints[leftPointer];
+                    leftPointer++;
+                }
+                else
+                {
+                    tempArray[rank] = ints[rightPointer];
+                    rightPointer++;
+                }
+                rank++;
+            }
+
+            while (leftPointer <= midPoint)
+            {
+                tempArray[rank] = ints[leftPointer];
+                leftPointer++;
+                rank++;
+            }
+
+            while (rightPointer <= right)
+            {
+                tempArray[rank] = ints[rightPointer];
+                rightPointer++;
+                rank++;
+            }
+
+            for (int i = 0; i < tempArray.Length; i ++)
+            {
+                ints[left+i] = tempArray[i];
+            }
+        }
     }
 }
